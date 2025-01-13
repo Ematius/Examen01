06b8f0eac491f57ef44b8930fc43f592295851fc
Emad <eksaber87@gmail.com>
2025-01-13 11:20:56 +0100

    Casi button

--- Examen/main.js
+++ Examen/main.js
@@ -1,4 +1,23 @@
 
 
-const form = document.querySelector('.form');
+const form = document.querySelectorAll('.form');
 const input = document.querySelector('.input');
+const buttons = document.querySelectorAll('.button');
+const ratingInput = document.getElementById("ratingInput");
+
+
+function handleButton() {
+    form.forEach((button) => {
+        button.addEventListener('click', (event) => {
+            event.preventDefault();
+            
+
+            
+           
+        });
+    });
+   
+}
+
+
+
