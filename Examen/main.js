2e05af122834654ea980a8fd7de1a6684b16b7f6
Emad <eksaber87@gmail.com>
2025-01-13 11:56:02 +0100

    fix button and create change .hero

--- Examen/main.js
+++ Examen/main.js
@@ -3,21 +3,46 @@
 const form = document.querySelectorAll('.form');
 const input = document.querySelector('.input');
 const buttons = document.querySelectorAll('.button');
-const ratingInput = document.getElementById("ratingInput");
+let selectedValue = '';
+
+const main = document.querySelector(".hero");
 
 
-function handleButton() {
-    form.forEach((button) => {
-        button.addEventListener('click', (event) => {
+
+
+
+
+
+function handleButton(){
+    buttons.forEach((button) => {
+        button.addEventListener("click", (event) => {
             event.preventDefault();
-            
+            selectedValue = button.getAttribute("value");
+            if (selectedValue !== '') {
+                main.innerHTML = /*html*/ `
+                        <header>
+                            <img src="img/illustration-thank-you.svg" alt="Imagen de agradecimiento">
+                            <div>
+                                <p>Seleccionaste ${selectedValue} de 5</p>
+                            </div>
+                        </header>
+                        <section>
+                            <h2>¡Gracias!</h2>
+                            <p>Agradecemos que te hayas tomado el tiempo de dar una calificación. Si alguna vez necesitas más ayuda, no dudes en volver a ponerte en contacto ocn nosotros</p>
+                        </section>
 
-            
-           
+                    `; }
+            console.log(selectedValue);
+
         });
     });
-   
 }
+
+handleButton();
+
+
+
+
 
 
 
