5b7b3dae2f3eb8fb291a60c8e7509f6c780aed56
Emad <eksaber87@gmail.com>
2025-01-13 13:31:56 +0100

    finish html and JS

--- Examen/main.js
+++ Examen/main.js
@@ -5,44 +5,41 @@ const input = document.querySelector('.input');
 const buttons = document.querySelectorAll('.button');
 let selectedValue = '';
 
-const main = document.querySelector(".hero");
 
 
-
-
-
-
-
-function handleButton(){
+function handleButton() {
     buttons.forEach((button) => {
         button.addEventListener("click", (event) => {
             event.preventDefault();
-            
+            buttons.forEach((button) => button.classList.remove("selected"));
+            button.classList.add("selected");
             selectedValue = button.getAttribute("value");
-            if (selectedValue !== '') {
-                main.innerHTML = /*html*/ `
-                        <header>
-                            <img src="img/illustration-thank-you.svg" alt="Imagen de agradecimiento">
-                            <div>
-                                <p>Seleccionaste ${selectedValue} de 5</p>
-                            </div>
-                        </header>
-                        <section>
-                            <h2>¡Gracias!</h2>
-                            <p>Agradecemos que te hayas tomado el tiempo de dar una calificación. Si alguna vez necesitas más ayuda, no dudes en volver a ponerte en contacto ocn nosotros</p>
-                        </section>
-
-                    `; }
-            console.log(selectedValue);
-
+            input.setAttribute("data-value", selectedValue); 
+            input.removeAttribute("disabled");
         });
+    });
+    input.addEventListener("click", (event) => {
+        event.preventDefault();
+        if (selectedValue !== "") {
+            const main = document.querySelector(".hero");
+            main.innerHTML = /*html*/ `
+            <header>
+                <img src="img/illustration-thank-you.svg" alt="Imagen de agradecimiento">
+                <div>
+                    <p>Seleccionaste ${selectedValue} de 5</p>
+                </div>
+            </header>
+            <section>
+                <h2>¡Gracias!</h2>
+                <p>Agradecemos que te hayas tomado el tiempo de dar una calificación. Si alguna vez necesitas más ayuda, no dudes en volver a ponerte en contacto con nosotros</p>
+            </section>
+        `;
+        }
     });
 }
 
 
 
-
 handleButton();
-
 
 
