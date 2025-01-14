57138a0e1fccd1c9790d8fcc9b7ae89195e9a4cd
Emad Kadyear <eksaber87@gmail.com>
2025-01-14 23:03:16 +0100

    Change innerHTML for Hidden

--- main.js
+++ main.js
@@ -2,7 +2,9 @@
 
 const form = document.querySelectorAll('.form');
 const input = document.querySelector('.input');
-const buttons = document.querySelectorAll('.button');
+const buttons = document.querySelectorAll(".button");
+const question = document.querySelector(".question");
+const answerSection = document.querySelector(".answer");
 let selectedValue = '';
 
 
@@ -16,25 +18,15 @@ function handleButton() {
             selectedValue = button.getAttribute("value");
             input.setAttribute("data-value", selectedValue); 
             input.removeAttribute("disabled");
+            console.log(selectedValue);
         });
     });
     input.addEventListener("click", (event) => {
         event.preventDefault();
-        if (selectedValue !== "") {
-            const main = document.querySelector(".hero");
-            main.innerHTML = /*html*/ `
-                <header class="secondaryHeader">
-                    <img src="img/illustration-thank-you.svg" alt="Imagen de agradecimiento">
-                    <div>
-                        <p>Seleccionaste ${selectedValue} de 5</p>
-                    </div>
-                </header>
-                <section class="secondarySection">
-                    <h2>¡Gracias!</h2>
-                    <p>Agradecemos que te hayas tomado el tiempo de dar una calificación.</p>
-                    <p>Si alguna vez necesitas más ayuda, no dudes en volver a ponerte en contacto con nosotros</p>
-                </section>
-        `;
+        if (selectedValue !== '') {
+            question.classList.toggle("hidden");
+            answerSection.removeAttribute("hidden");
+            answerSection.querySelector("p").textContent = `Seleccionaste ${selectedValue} de 5`;
         }
     });
 }
